b6245a17c7f07f0ff514b3baef465d7eaf37540e
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2021-10-17 09:29:53 +0100

    Removed onscroll from useRefscroll hook

--- src/hooks/useRefScrollProgress.ts
+++ src/hooks/useRefScrollProgress.ts
@@ -9,14 +9,13 @@ const useRefScrollProgress = () => {
     if (!ref.current) {
       return;
     }
-    window.addEventListener("scroll", () => {
-      //@ts-ignore
-      const rect = ref.current.getBoundingClientRect()!;
-      const scrollTop = window.scrollY || document.documentElement.scrollTop;
-      const offsetTop = rect.top + scrollTop;
-      setStart(offsetTop / document.body.clientHeight);
-      setEnd((offsetTop + rect.height) / document.body.clientHeight);
-    });
+
+    //@ts-ignore
+    const rect = ref.current.getBoundingClientRect()!;
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    const offsetTop = rect.top + scrollTop;
+    setStart(offsetTop / document.body.clientHeight);
+    setEnd((offsetTop + rect.height) / document.body.clientHeight);
   }, [start, end, ref]);
   return { ref, start, end };
 };
