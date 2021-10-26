dd6f47ce37ff994875d140b4328a0894963b8e15
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2021-10-26 12:00:17 +0100

    Copied service worker to public directory

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 
 import { persistor, store } from "./app/store";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./service-worker";
 
 import "./index.css";
 import App from "./App";
