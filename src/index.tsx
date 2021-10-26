bfa28bef52fcec2934750f705fe4d30af6bd3ec4
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2021-10-26 12:30:14 +0100

    Added pwa-config

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 
 import { persistor, store } from "./app/store";
-import * as serviceWorker from "./service-worker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import "./index.css";
 import App from "./App";
@@ -21,8 +21,12 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.unregister();
